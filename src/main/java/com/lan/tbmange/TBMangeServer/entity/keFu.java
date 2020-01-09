04a0a6b8925ae938844a75141a120bea70949a24
13482 <13482@LAPTOP-MJGGQ20U>
2020-01-09 18:00:54 +0800

    客服实体类

---
+++ src/main/java/com/lan/tbmange/TBMangeServer/entity/keFu.java
@@ -0,0 +1,35 @@
+package com.lan.tbmange.TBMangeServer.entity;
+
+public class keFu {
+
+	
+	String username = "";
+	String password = "";
+	
+	public keFu(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
+	public keFu(String username) {
+		super();
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
