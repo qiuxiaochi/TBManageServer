183750ec635fd6a49c01d356ec538da783722240
13482 <13482@LAPTOP-MJGGQ20U>
2020-01-09 18:00:08 +0800

    技术管理数据库dao的一个实现类

---
+++ src/main/java/com/lan/tbmange/TBMangeServer/db/dbservice/JishuDaoImpl.java
@@ -0,0 +1,72 @@
+package com.lan.tbmange.TBMangeServer.db.dbservice;
+
+import java.util.Vector;
+
+import com.lan.tbmange.TBMangeServer.db.dao.JishuDao;
+import com.lan.tbmange.TBMangeServer.entity.Jishu;
+import com.lan.tbmange.TBMangeServer.entity.Vote;
+import com.lan.tbmange.TBMangeServer.entity.keFu;
+
+public class JishuDaoImpl implements JishuDao {
+
+	@Override
+	public String addJishu(keFu keFu, Jishu jishu) {
+		
+		return null;
+	}
+
+	@Override
+	public String supplyJishu(keFu keFu, Jishu jishu) {
+		
+		return null;
+	}
+
+	@Override
+	public String voteJishu(Vote vote) {
+		
+		return null;
+	}
+
+	@Override
+	public Jishu getJishuByQq(String qq) {
+		
+		return null;
+	}
+
+	@Override
+	public Jishu getJishuByWx(String wx) {
+		
+		return null;
+	}
+
+	@Override
+	public Vector<Jishu> getJishuListBySkill(String Skill) {
+		
+		return null;
+	}
+
+	@Override
+	public Vector<Jishu> getOnlyYesJishus() {
+		
+		return null;
+	}
+
+	@Override
+	public Vector<Jishu> getOnlyNoJishus() {
+		
+		return null;
+	}
+
+	@Override
+	public Vector<Jishu> getYesJishus() {
+	
+		return null;
+	}
+
+	@Override
+	public Vector<Jishu> getNoJishus() {
+		
+		return null;
+	}
+
+}
