389d26587ffccdde8f088a9b9a042203bc022eb5
13482 <13482@LAPTOP-MJGGQ20U>
2020-01-09 18:00:47 +0800

    技术信息管理表现层接口的实现类

---
+++ src/main/java/com/lan/tbmange/TBMangeServer/verticleServer/service/jsMgService/JsMgServiceImpl.java
@@ -0,0 +1,80 @@
+package com.lan.tbmange.TBMangeServer.verticleServer.service.jsMgService;
+
+import java.util.Vector;
+
+import com.lan.tbmange.TBMangeServer.entity.Jishu;
+import com.lan.tbmange.TBMangeServer.entity.Vote;
+import com.lan.tbmange.TBMangeServer.entity.keFu;
+
+public class JsMgServiceImpl implements IJsMgService {
+
+	@Override
+	public String addJishu(keFu keFu, Jishu jishu) {
+		
+		return null;
+	}
+	
+	
+	@Override
+	public String voteJishu(Vote vote) {
+		
+		return null;
+	}
+
+
+	@Override
+	public String supplyJishu(keFu keFu, Jishu jishu) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Jishu getJishuByQq(String qq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Jishu getJishuByWx(String wx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Vector<Jishu> getJishuListBySkill(String Skill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Vector<Jishu> getOnlyYesJishus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Vector<Jishu> getOnlyNoJishus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Vector<Jishu> getYesJishus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Vector<Jishu> getNoJishus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
