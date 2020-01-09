8d1fc2c2fcbfae32708e3b99043aab2cc1d93202
13482 <13482@LAPTOP-MJGGQ20U>
2020-01-09 17:57:17 +0800

    获得只有好评的技术

---
+++ src/main/java/com/lan/tbmange/TBMangeServer/verticleServer/handlers/jishuMgHandlers/GetOnlyYesJishusHandler.java
@@ -0,0 +1,56 @@
+package com.lan.tbmange.TBMangeServer.verticleServer.handlers.jishuMgHandlers;
+
+import java.util.Vector;
+
+import com.lan.tbmange.TBMangeServer.entity.Jishu;
+import com.lan.tbmange.TBMangeServer.threadUtil.FixedThreadPool;
+import com.lan.tbmange.TBMangeServer.verticleServer.httpUtil.HttpUtil;
+import com.lan.tbmange.TBMangeServer.verticleServer.service.jsMgService.IJsMgService;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.ext.web.RoutingContext;
+
+/*
+ * 获得只有好评的技术
+ */
+public class GetOnlyYesJishusHandler implements Handler<RoutingContext> {
+
+	private IJsMgService service;
+
+	public GetOnlyYesJishusHandler(IJsMgService userService) {
+		this.service = userService;
+	}
+
+	@Override
+	public void handle(RoutingContext ctx) {
+
+		JsonObject bodyAsJson = ctx.getBodyAsJson();
+		FixedThreadPool.threadPool.submit(new Runnable() {
+
+			@Override
+			public void run() {
+
+				Vector<Jishu> jishus = service.getOnlyYesJishus();
+
+				JsonObject jsonObject = new JsonObject();
+
+				if (jishus == null || jishus.size() == 0) {
+					
+					jsonObject.put("status", "error");
+					jsonObject.put("message", "该技术不存在!");
+					HttpUtil.resp(ctx.request(), jsonObject);
+
+				} else {
+
+					jsonObject.put("status", "ok");
+					jsonObject.put("jishus", jishus);
+					HttpUtil.resp(ctx.request(), jsonObject);
+
+				}
+
+			}
+		});
+
+	}
+
+}
