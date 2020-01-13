0040d4a91974ad4da7f1f7e927290ebaaccc9d98
13482 <13482@LAPTOP-MJGGQ20U.bbrouter>
2020-01-13 13:36:44 +0800

    技术管理 表现层完成

--- src/main/java/com/lan/tbmange/TBMangeServer/verticleServer/service/jsMgService/JsMgServiceImpl.java
+++ src/main/java/com/lan/tbmange/TBMangeServer/verticleServer/service/jsMgService/JsMgServiceImpl.java
@@ -2,6 +2,7 @@ package com.lan.tbmange.TBMangeServer.verticleServer.service.jsMgService;
 
 import java.util.Vector;
 
+import com.lan.tbmange.TBMangeServer.db.daoFactory.JishuDaoFactory;
 import com.lan.tbmange.TBMangeServer.entity.Jishu;
 import com.lan.tbmange.TBMangeServer.entity.Vote;
 import com.lan.tbmange.TBMangeServer.entity.keFu;
@@ -11,70 +12,70 @@ public class JsMgServiceImpl implements IJsMgService {
 	@Override
 	public String addJishu(keFu keFu, Jishu jishu) {
 		
-		return null;
+		return JishuDaoFactory.getJishuDao().addJishu(keFu, jishu);
 	}
 	
 	
 	@Override
 	public String voteJishu(Vote vote) {
 		
-		return null;
+		return JishuDaoFactory.getJishuDao().voteJishu(vote);
 	}
 
 
 	@Override
 	public String supplyJishu(keFu keFu, Jishu jishu) {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().supplyJishu(keFu, jishu);
 	}
 
 
 	@Override
 	public Jishu getJishuByQq(String qq) {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getJishuByQq(qq);
 	}
 
 
 	@Override
 	public Jishu getJishuByWx(String wx) {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getJishuByWx(wx);
 	}
 
 
 	@Override
 	public Vector<Jishu> getJishuListBySkill(String Skill) {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getJishuListBySkill(Skill);
 	}
 
 
 	@Override
 	public Vector<Jishu> getOnlyYesJishus() {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getOnlyYesJishus();
 	}
 
 
 	@Override
 	public Vector<Jishu> getOnlyNoJishus() {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getOnlyNoJishus();
 	}
 
 
 	@Override
 	public Vector<Jishu> getYesJishus() {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getYesJishus();
 	}
 
 
 	@Override
 	public Vector<Jishu> getNoJishus() {
 		// TODO Auto-generated method stub
-		return null;
+		return JishuDaoFactory.getJishuDao().getNoJishus();
 	}
 
 }
